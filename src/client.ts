f85abd04c7ef0158d00f946b78e14dcf3ecf0ad2
juji <jujiyangasli@gmail.com>
2024-06-07 21:26:07 +0700

    fix: comment out test from client

--- src/client.ts
+++ src/client.ts
@@ -93,7 +93,7 @@ export function box(str: string){
 }
 
 // 9999999 is good enough, i think
-for(let i = 0; i < 9999999; i++){
-  success( i + ' Example content ' + Math.random() )
-}
-console.log('itt done')
+// for(let i = 0; i < 9999999; i++){
+//   success( i + ' Example content ' + Math.random() )
+// }
+// console.log('itt done')
